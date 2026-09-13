a9dce2c94159b47fab1156ad322158a840824bd1
junaoki8823-cmd <junaoki8823@gmail.com>
2026-09-13 22:37:37 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='keisan-battle-v2';const FILES=['./','./index.html','./manifest.webmanifest','./sw.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
